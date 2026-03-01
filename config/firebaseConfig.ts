cd984ed2cb5f33bec44f461c7b7c89778c273a40
IvanBanal <ibanal@rrc.ca>
2026-03-01 17:15:06 -0600

    Added .json file for my private key to serviceAccount

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-import * as serviceAccount from "./<your-service-account-file>.json";
+import * as serviceAccount from "comp-3018-module-3-assig-eed36-firebase-adminsdk-fbsvc-27af21fafd.json";
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
