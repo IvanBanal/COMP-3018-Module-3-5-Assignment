d4a906f1409d66b58d98a483e11596068339e84f
IvanBanal <ibanal@rrc.ca>
2026-03-01 16:42:10 -0600

    Added capacity validation for createEventSchema.

--- src/api/v1/validation/eventSchema.ts
+++ src/api/v1/validation/eventSchema.ts
@@ -4,4 +4,7 @@ export const createEventSchema = Joi.object({
     name: Joi.string().min(3).required(),
     
     date: Joi.date().iso().greater("now").required(),
+
+    capacity: Joi.number().integer().min(5).required(),
+    
 })
