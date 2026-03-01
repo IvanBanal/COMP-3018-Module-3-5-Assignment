f4192e1556faf35c54956a1138a1b7a161ea133e
IvanBanal <ibanal@rrc.ca>
2026-03-01 17:27:57 -0600

    Added createEventRepo function for eventRepositoy.ts.

--- src/api/v1/repositories/eventRepository.ts
+++ src/api/v1/repositories/eventRepository.ts
@@ -1,1 +1,19 @@
-import { db } from 
+import { db } from "../../../../config/firebaseConfig";
+import { Event } from "../models/eventModel";
+
+const COLLECTION = "events";
+
+/**
+ * This will create a new event in Firestore.
+ * @param event - Event object to save.
+ * @returns The saved Event.
+ */
+export const createEventRepo = async (event: Event): Promise<Event> => {
+  const docRef = db.collection(COLLECTION).doc(event.id);
+  await docRef.set(event);
+  return event;
+};
+
+
+
+
